bb39b7a4dc561329e18399ec226f6eed971bdd04
BobbyLeeSH <tkdgus1797@gmail.com>
2021-02-08 17:22:11 +0900

    FEAT: add styled-component and reset css

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {connect, ConnectedProps} from "react-redux";
 import {addTest} from "../redux/tests/actions";
+import styled from "styled-components";
 
 interface RootState {
     count: number
@@ -12,7 +13,7 @@ const mapStateToProps = (state: RootState) => {
     }
 }
 
-const mapDispatchToProps =  {
+const mapDispatchToProps = {
     addTest
 }
 
@@ -26,14 +27,46 @@ type Props = PropsFromRedux & {
     backgroundColor: string
 }
 
+const MainContainer = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background: #e9ecef;
+`
+
+const BodyContainer = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  border-radius: 16px;
+  background: white;
+  width: 80vw;
+  height: 90vh;
+  font-size: 2rem;
+`
+
+const FlexDiv = styled.div`
+  margin: 1rem;
+  display: flex;
+  justify-content: center;
+`
+
 const Test = ({count, addTest}: Props) => {
     return (
-        <h2>
-            hello this is test: {count}
-            <div>
-                <button onClick={() => addTest()}>Add Test</button>
-            </div>
-        </h2>
+        <MainContainer>
+            <BodyContainer>
+                <FlexDiv>Hello</FlexDiv>
+                <FlexDiv>
+                    hello this is test: {count}
+                </FlexDiv>
+                <FlexDiv>
+                    <button onClick={() => addTest()}>Add Test</button>
+                </FlexDiv>
+            </BodyContainer>
+        </MainContainer>
+
     );
 }
 
